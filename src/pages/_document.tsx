d20ecabef34d82fac2b30725cf40dbad23883813
Lucas Juste <lu.scatollini@hotmail.com>
2021-11-24 00:39:17 -0300

    New Deploying Chakra SSR

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,6 +1,34 @@
-import Document, { Html, Head, Main, NextScript } from "next/document";
+import Document, {
+  Html,
+  Head,
+  Main,
+  NextScript,
+  DocumentContext,
+} from "next/document";
+import * as React from "react";
+
+import emotionCache from "../../packages/v7/lib/emotion-cache";
+
+import createEmotionServer from "@emotion/server/create-instance";
+
+const { extractCritical } = createEmotionServer(emotionCache);
 
 export default class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    const styles = extractCritical(initialProps.html);
+    return {
+      ...initialProps,
+      styles: [
+        initialProps.styles,
+        <style
+          key="emotion-css"
+          dangerouslySetInnerHTML={{ __html: styles.css }}
+          data-emotion-css={styles.ids.join(" ")}
+        />,
+      ],
+    };
+  }
   render() {
     return (
       <Html>
